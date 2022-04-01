63c37b77e97a9b96b01a8fcb8d9de14e332558a0
Dyego Aurelo <dyegoaurelio@gmail.com>
2022-04-01 12:37:11 -0300

    calculando corretamente o tamanho pelo bitrate

--- __main__.py
+++ __main__.py
@@ -2,26 +2,24 @@ import sys
 from subprocess import run
 from os import system, path
 from math import ceil
-MAX_SIZE = 15.5 * 1024 * 1024
-filename = sys.argv[-1]
+
+MAX_BITS = 15.75 * 8000
+filepath = sys.argv[-1]
 
 
-# file_size = path.getsize(filename)
-
-# proportion = ceil(file_size/MAX_SIZE)
-
-proportion = 6
+def get_duration(path):
+    return float(run(f'ffprobe -i {path} -show_entries format=duration -v quiet -of csv="p=0"', capture_output=True, shell=True).stdout)
 
 
-system(
-    f'ffmpeg -i {filename} -vf "scale=trunc(iw/{proportion})*2:trunc(ih/{proportion})*2" ' \
-        '-c:v libx264 -crf 28 -acodec aac -profile:v baseline -level 3.0 -pix_fmt yuv420p   output5.mp4'
-)
+file_size = path.getsize(filepath)
+duration = get_duration(filepath)
 
-# %%
-# num = 17
+bitrate = ceil(MAX_BITS / duration) - 128
 
-# MAX_DIV = 5
-# extra = num % MAX_DIV
-# qtd_max = int((num - extra)/MAX_DIV)
-# print(extra, qtd_max)
+
+filename = filepath.split('/')[-1]
+file_extension = filename.split('.')[-1]
+file_title = filename[:-len(file_extension) - 1]
+
+system(f'ffmpeg -y -i {filepath} -c:v libx264 -b:v {bitrate}k -pass 1 -vsync cfr -f null /dev/null && \
+ffmpeg -i {filepath} -c:v libx264 -b:v {bitrate}k -pass 2 -c:a aac -b:a 128k {file_title}-WHATSAPP.mp4')
